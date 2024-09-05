83bbd0aae09583ec91639bc2b35b59924d29d0a8
JaSON Rete <jsonrete@gmail.com>
2024-09-05 14:05:45 -0400

    added given quiz solution

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/ORDER_BY.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/ORDER_BY.sql
@@ -16,3 +16,21 @@ ORDER BY total_amt_usd
 LIMIT 20;
 
 
+-- quiz solution:
+
+-- Solutions to previous ORDER BY questions
+-- Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+SELECT id, occurred_at, total_amt_usd
+FROM orders
+ORDER BY occurred_at
+LIMIT 10;
+-- Write a query to return the top 5 orders in terms of the largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC 
+LIMIT 5;
+-- Write a query to return the lowest 20 orders in terms of the smallest total_amt_usd. Include the id, account_id, and total_amt_usd.
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd
+LIMIT 20;
