4adcff734995c33c87b70a2d2657d2ee19797120
JaSON Rete <jsonrete@gmail.com>
2024-09-05 18:15:34 -0400

    given solution

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/WHERE.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/WHERE.sql
@@ -14,3 +14,15 @@ SELECT *
 FROM orders
 WHERE total_amt_usd <= 500
 LIMIT 10;
+
+-- given solution:
+-- Solution from previous WHERE Questions
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
