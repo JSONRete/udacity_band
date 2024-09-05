e348075d3fcdd4e83e1129594b7497e23d861cd0
JaSON Rete <jsonrete@gmail.com>
2024-09-05 14:23:27 -0400

    added answer

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/ORDER_BY_Part_II.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/ORDER_BY_Part_II.sql
@@ -3,3 +3,10 @@
 -- Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
 -- Compare the results of these two queries above. How are the results different when you switch the column you sort on first?
 -- Below is the workspace where you can write your queries. Once you write your queries against the above mentioned questions, you can click on the EVALUATE button at the bottom of the workspace. And the workspaces have the required tables loaded already.
+
+-- my 1st answer for 1st question:
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id ASC, total_amt_usd DESC
+
