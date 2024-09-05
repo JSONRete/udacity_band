b5d7a6da229ef5df86f2fec5ad2280db257602fb
JaSON Rete <jsonrete@gmail.com>
2024-09-05 18:04:53 -0400

    given question

---
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/WHERE.sql
@@ -0,0 +1,6 @@
+-- given question:
+-- Write a query that:
+
+-- Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+
+-- Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
