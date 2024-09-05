7eabca15c24f04c2efe3d3fd5a3e512a910b3959
JaSON Rete <jsonrete@gmail.com>
2024-09-05 14:12:19 -0400

    added question

---
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/ORDER_BY_Part_II.sql
@@ -0,0 +1,5 @@
+-- Questions
+-- Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).
+-- Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
+-- Compare the results of these two queries above. How are the results different when you switch the column you sort on first?
+-- Below is the workspace where you can write your queries. Once you write your queries against the above mentioned questions, you can click on the EVALUATE button at the bottom of the workspace. And the workspaces have the required tables loaded already.
