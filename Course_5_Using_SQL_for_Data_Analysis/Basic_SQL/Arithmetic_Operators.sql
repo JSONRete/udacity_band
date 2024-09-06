1bf5c682407c6852aa7fcf4af2a25a17ae335dcf
JaSON Rete <jsonrete@gmail.com>
2024-09-06 09:58:00 -0400

    this is my 2nd question solution

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/Arithmetic_Operators.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/Arithmetic_Operators.sql
@@ -11,3 +11,10 @@ LIMIT 10;
 SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
 FROM orders
 LIMIT 10;
+
+-- my 2nd answer solution:
+
+SELECT id, account_id, poster_amt_usd / (standard_amt_usd + gloss_amt_usd + poster_amt_usd) * 100 AS poster_paper_percentage
+FROM orders
+LIMIT 10;
+
