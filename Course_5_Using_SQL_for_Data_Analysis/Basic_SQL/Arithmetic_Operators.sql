3077b12ada1a8decee6d6d35820034f42157bcbf
JaSON Rete <jsonrete@gmail.com>
2024-09-06 05:21:10 -0400

    my answer one

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/Arithmetic_Operators.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/Arithmetic_Operators.sql
@@ -5,3 +5,9 @@
 SELECT id, (standard_amt_usd/total_amt_usd)*100 AS std_percent, total_amt_usd
 FROM orders
 LIMIT 10;
+
+
+-- entered solution to the quiz for question one
+SELECT id, account_id, standard_amt_usd/standard_qty AS unit_price
+FROM orders
+LIMIT 10;
