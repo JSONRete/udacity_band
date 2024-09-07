cb788360da1d3ddda3deaa2d350c8d9f78922a00
JaSON Rete <jsonrete@gmail.com>
2024-09-07 05:14:38 -0400

    quiz questions added

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/AND_and_BETWEEN.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/AND_and_BETWEEN.sql
@@ -15,3 +15,14 @@ SELECT *
 FROM orders
 WHERE occurred_at BETWEEN '2016-04-01' AND '2016-10-01'
 ORDER BY occurred_at
+
+-- quiz questions:
+
+-- Questions using AND and BETWEEN operators
+-- Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.
+
+-- Using the accounts table, find all the companies whose names do not start with 'C' and end with 's'.
+
+-- When you use the BETWEEN operator in SQL, do the results include the values of your endpoints, or not? Figure out the answer to this important question by writing a query that displays the order date and gloss_qty data for all orders where gloss_qty is between 24 and 29. Then look at your output to see if the BETWEEN operator included the begin and end values or not.
+
+-- Use the web_events table to find all information regarding individuals who were contacted via the organic or adwords channels, and started their account at any point in 2016, sorted from newest to oldest.
