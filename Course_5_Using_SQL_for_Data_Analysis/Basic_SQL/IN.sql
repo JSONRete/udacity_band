8b6ebc3452e5d946e3fc6c2cf35d2cc289d642ab
JaSON Rete <jsonrete@gmail.com>
2024-09-07 03:18:37 -0400

    added quiz solutions

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/IN.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/IN.sql
@@ -19,3 +19,13 @@ WHERE name IN ('Walmart', 'Target', 'Nordstrom');
 SELECT *
 FROM web_events
 WHERE channel IN ('organic', 'adwords')
+
+-- quiz solutions:
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
