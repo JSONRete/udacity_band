44b372c7915ed6272623919e273d5a707755593c
JaSON Rete <jsonrete@gmail.com>
2024-09-07 01:20:29 -0400

    working on quiz ex.1

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/LIKE.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/LIKE.sql
@@ -3,3 +3,19 @@
 SELECT *
 FROM accounts
 WHERE website LIKE '%google%';
+
+
+-- Questions using the LIKE operator
+-- Use the accounts table to find
+
+-- All the companies whose names start with 'C'.
+
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%';
+
+-- All companies whose names contain the string 'one' somewhere in the name.
+
+-- All companies whose names end with 's'.
+
+-- You can try executing these queries in the workspace given below.
