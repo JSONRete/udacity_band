dc8378d1db4ed298ad802584299ca5bebfad8ed2
JaSON Rete <jsonrete@gmail.com>
2024-09-07 04:06:04 -0400

    added quiz solutions

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/NOT.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/NOT.sql
@@ -50,3 +50,27 @@ WHERE name NOT LIKE '%one%';
 SELECT name
 FROM accounts
 WHERE name NOT LIKE '%s';
+
+
+-- given quiz solutions:
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords');
+
+-- Solutions to NOT LIKE Questions
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%';
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%one%';
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE '%s';
