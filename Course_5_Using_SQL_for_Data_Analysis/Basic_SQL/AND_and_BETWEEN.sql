111b20143e7d9e3ad1e4e1c7f2e2f186f39ed894
JaSON Rete <jsonrete@gmail.com>
2024-09-07 05:29:37 -0400

    added answer to 1

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/AND_and_BETWEEN.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/AND_and_BETWEEN.sql
@@ -20,6 +20,9 @@ ORDER BY occurred_at
 
 -- Questions using AND and BETWEEN operators
 -- Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0
 
 -- Using the accounts table, find all the companies whose names do not start with 'C' and end with 's'.
 
