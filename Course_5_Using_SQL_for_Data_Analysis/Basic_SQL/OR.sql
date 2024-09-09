84df42d48695f0bd1faa78a621b87ec95b8e5d84
JaSON Rete <jsonrete@gmail.com>
2024-09-08 21:50:06 -0400

    added code from video and questions

--- Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/OR.sql
+++ Course_5_Using_SQL_for_Data_Analysis/Basic_SQL/OR.sql
@@ -16,3 +16,12 @@ SELECT account_id,
 FROM orders
 WHERE (standard_qty = 0 OR gloss_qty = 0 OR poster_qty = 0)
 AND occurred_at >= '2016-10-01'
+
+-- Questions using the OR operator
+Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.
+
+
+Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.
+
+
+Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
