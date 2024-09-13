346808f7c440b6e43ef851d0ec921c3d8bab4dcb
JaSON Rete <jsonrete@gmail.com>
2024-09-13 03:52:11 -0400

    question 1 entered

---
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Questions_Part_I.sql
@@ -0,0 +1,7 @@
+/*
+Question 01
+Provide a table for all web_events associated with the account name of Walmart. 
+There should be three columns. Be sure to include the primary_poc, time of the event
+and the channel for each event. Additionally, you might choose to add a fourth column
+to ensure only Walmart events were chosen.
+*/
