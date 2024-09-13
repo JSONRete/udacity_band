1a73a0f30f76b86514365098236e0b7118fb3b3c
JaSON Rete <jsonrete@gmail.com>
2024-09-13 02:47:19 -0400

    testing on my own

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/Alias.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/Alias.sql
@@ -3,3 +3,10 @@ SELECT o.*, a.*
 FROM orders o
 JOIN accounts a
 ON o.account_id = a.id
+
+-- the orders 'true' id is 'overridden' by the accounts id until I 'AS' the order id name
+
+SELECT o.id order_id, a.id accounts
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
