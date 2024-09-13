91505da9f33b82ba442fb238fb9f419fa5e5c5b3
JaSON Rete <jsonrete@gmail.com>
2024-09-12 23:59:49 -0400

    new given query

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Revisited.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Revisited.sql
@@ -8,3 +8,8 @@ ON accounts.id = orders.account_id
 
 -- given 2nd test code:
 SELECT web_events.channel, accounts.name, orders.total
+FROM web_events
+JOIN accounts
+ON web_events.account_id = accounts.id
+JOIN orders
+ON accounts.id = orders.account_id
