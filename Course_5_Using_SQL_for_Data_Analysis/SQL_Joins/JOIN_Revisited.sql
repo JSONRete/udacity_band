500436fe1293d628bfc8dc637a5dabc363165c67
JaSON Rete <jsonrete@gmail.com>
2024-09-12 23:59:16 -0400

    additional changed line

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Revisited.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Revisited.sql
@@ -5,3 +5,6 @@ JOIN accounts
 ON web_events.account_id = accounts.id
 JOIN orders
 ON accounts.id = orders.account_id
+
+-- given 2nd test code:
+SELECT web_events.channel, accounts.name, orders.total
