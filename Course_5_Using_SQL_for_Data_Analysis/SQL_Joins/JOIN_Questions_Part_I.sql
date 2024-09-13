f9708794a3f98d1926b0c0d700317d86faf43474
JaSON Rete <jsonrete@gmail.com>
2024-09-13 04:12:56 -0400

    my question 1 attempt

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Questions_Part_I.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/JOIN_Questions_Part_I.sql
@@ -5,3 +5,9 @@ There should be three columns. Be sure to include the primary_poc, time of the e
 and the channel for each event. Additionally, you might choose to add a fourth column
 to ensure only Walmart events were chosen.
 */
+
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM WEB_EVENTS w
+JOIN accounts a
+ON a.id = w.account_id
+WHERE a.name = 'Walmart';
