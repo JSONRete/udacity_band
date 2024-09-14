ab81a94b0773132b98b58a40f87e9f39fc54ad27
JaSON Rete <jsonrete@gmail.com>
2024-09-14 10:05:32 -0400

    added my 3rd answer

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/Last_Check.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Joins/Last_Check.sql
@@ -34,7 +34,30 @@ WHERE r.name = 'Midwest' AND s.name LIKE 'S%'
 ORDER BY a.name
 
 -- 03 Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to the account name.
+
+-- my answer:
+SELECT r.name region_name, s.name sales_rep, a.name acct_name
+FROM accounts a
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE r.name = 'Midwest' AND s.name LIKE '%K%' AND s.name NOT LIKE 'K%'
+ORDER BY a.name
+
 -- 04 Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100. Your final table should have 3 columns: region name, account name, and unit price. In order to avoid a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+
+-- my answer:
+SELECT r.name region_name, a.name acct, o.total_amt_usd/(total+0.01) unit_price
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id
+WHERE standard_qty > 100
+
 -- 05 Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01)).
 -- 06 Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the largest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01)).
 -- 07 What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values.
