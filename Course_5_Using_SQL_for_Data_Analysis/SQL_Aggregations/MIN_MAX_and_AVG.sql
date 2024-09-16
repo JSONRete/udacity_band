e0dfb2e6e25684201e6c61e6d06485cd3c1eca08
JaSON Rete <jsonrete@gmail.com>
2024-09-16 18:28:17 -0400

    added given questions

---
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Aggregations/MIN_MAX_and_AVG.sql
@@ -0,0 +1,11 @@
+-- 01 When was the earliest order ever placed? You only need to return the date.
+
+-- 02 Try performing the same query as in question 1 without using an aggregation function.
+
+-- 03 When did the most recent (latest) web_event occur?
+
+-- 04 Try to perform the result of the previous query without using an aggregation function.
+
+-- 05 Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+
+-- 06 Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
