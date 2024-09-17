83f4d605565fef4c6fda3aec090742924c693399
JaSON Rete <jsonrete@gmail.com>
2024-09-16 22:12:47 -0400

    my answers

--- Course_5_Using_SQL_for_Data_Analysis/SQL_Aggregations/DISTINCT.sql
+++ Course_5_Using_SQL_for_Data_Analysis/SQL_Aggregations/DISTINCT.sql
@@ -17,3 +17,31 @@ SELECT DISTINCT account_id,
                 channel
 FROM web_events
 ORDER BY account_id
+
+-- QUIZ:
+-- 01 Use DISTINCT to test if there are any accounts associated with more than one region.
+-- my answer:
+SELECT r.id as "Region ID",  r.name as "Region", a.id as "Account ID", a.name as "Account Name"
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id
+
+-- compared to:
+
+SELECT DISTINCT a
+FROM accounts a
+
+-- 02 Have any sales reps worked on more than one account?
+-- my answer:
+SELECT s.id "ID#", s.name "Name", COUNT(s.name) "Number of Accounts"
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.id, s.name
+ORDER BY "Number of Accounts";
+
+-- compared to:
+SELECT DISTINCT s
+FROM sales_reps s
